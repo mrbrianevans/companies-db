1b2d8c6cc920e09ece1381146aa5a157e5d8254f
Brian Evans <ebrian101@gmail.com>
2022-07-01 09:59:04 +0100

    Benchmarked split2 for parsing NDJSON
    Split2 is faster than nodejs built-in createInterface
    
    Signed-off-by: Brian Evans <ebrian101@gmail.com>

--- crawler/pscSchema.ts
+++ crawler/pscSchema.ts
@@ -1,53 +1,47 @@
 /*
-Generate schema and type defs for PSC bulk file
+
+  Benchmark split2 against nodejs built-in createInterface for parsing ndjson.
+
+  For parsing a file of 500,000 lines:
+   - split2:          5.8s
+   - createInterface: 7.3s
+
  */
-
-
 import {createReadStream} from "fs";
 import {createInterface} from "readline";
-import {Readable} from "node:stream";
-import {writeFile} from "fs/promises";
-import {resolve} from "path";
-import JsonToTS from "json-to-ts";
-import {createCompoundSchema,extendSchema} from "genson-js";
+import split2 from 'split2'
+import {Readable} from "stream";
+import {setTimeout} from "timers/promises";
 
-/*
+const sampleFile = 'C:\\Users\\bme\\projects\\companiesv2\\services\\personsWithSignificantControl\\loadBulk\\downloads\\unzipped\\2022-06-30\\psc-snapshot-2022-06-30_1of21.txt'
 
-Generate JSON schemas for the PSC bulk file. There are multiple types of records in the bulk file:
 
-kind:
-    | 'individual-person-with-significant-control'
-    | 'legal-person-person-with-significant-control'
-    | 'corporate-entity-person-with-significant-control'
-    | 'super-secure-person-with-significant-control'
-    | 'persons-with-significant-control-statement'
-    | 'exemptions'
-    | "totals#persons-of-significant-control-snapshot"
- */
+async function testSplit2(filename){
+  const file = createReadStream(filename)
+  console.time('Count records with split2')
+  let counter = await file.pipe(split2(JSON.parse))
+    .reduce((prev)=>prev+1, 0)
+  console.timeEnd('Count records with split2')
+  console.log("Counted", counter, 'lines with split2')
+}
 
-const name = 'bulkFilePscExemptions'
-const kind = 'exemptions'
-const sampleFile = 'C:\\Users\\bme\\projects\\companiesv2\\services\\personsWithSignificantControl\\loadBulk\\downloads\\unzipped\\2022-06-30\\psc-snapshot-2022-06-30_21of21.txt'
 
-const file = createReadStream(sampleFile)
-const lines = createInterface({input: file})
-console.time('Generate schema')
-let counter = 0
-const schema = await Readable.from(lines)
-  //@ts-ignore
-  .map(JSON.parse, {concurrency: 8})
-  .filter(e=>e.data.kind === kind, {concurrency: 8}).take(100_000)
-  .reduce((previousValue, currentValue)=> {
-    counter++
-    return extendSchema(previousValue, currentValue)
-  }, { type: 'object' }) // problem with this is that top level items aren't required because the first item doesn't have anything.
-console.log('Generated',name, 'schema based on', counter, 'entities')
-console.timeEnd('Generate schema')
+async function testInterface(filename){
+  const file = createReadStream(filename)
+  const lines = createInterface({input: file})
+  console.time('Count records with interface')
+  let counter = await Readable.from(lines)
+    //@ts-ignore
+    .map(JSON.parse, {concurrency: 8})
+    .reduce((prev)=>prev+1, 0)
+  console.timeEnd('Count records with interface')
+  console.log("Counted", counter, 'lines with interface')
+}
 
-await writeFile(resolve(`../services/personsWithSignificantControl/loadBulk/bulkFileSchemas/${name}.ts`), `
-import {FromSchema} from "json-schema-to-ts";
+await testSplit2(sampleFile)
+await testInterface(sampleFile)
 
-const ${name}Schema = ${JSON.stringify(schema, null, 2)} as const
+await setTimeout(2000)
 
-export type ${name[0].toUpperCase()}${name.slice(1)} = FromSchema<typeof ${name}Schema>
-`)
+await testInterface(sampleFile)
+await testSplit2(sampleFile)
